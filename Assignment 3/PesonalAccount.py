02967f66953ec6394c3cd8f9cd0ff5eb5b9e4e2f
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-05 14:46:03 +0600

    amount class

--- Assignment 3/PesonalAccount.py
+++ Assignment 3/PesonalAccount.py
@@ -23,3 +23,42 @@ class PersonalAccount:
         transaction = Amount(amount, "DEPOSIT")
         self.transactions.append(transaction)
         self.balance += amount
+
+    def withdraw(self, amount: float):
+        if amount <= 0:
+            raise ValueError("um must be greater than 0")
+        if amount > self.balance:
+            raise ValueError("NONE")
+        transaction = Amount(amount, "WITHDRAWAL")
+        self.transactions.append(transaction)
+        self.balance -= amount
+
+    def print_transaction_history(self):
+        if not self.transactions:
+            print("none transaction")
+        else:
+            for transaction in self.transactions:
+                print(transaction)
+
+    def get_balance(self):
+        return self.balance
+
+    def get_account_number(self):
+        return self.account_number
+
+    def set_account_number(self, account_number: int):
+        self.account_number = account_number
+
+    def get_account_holder(self):
+        return self.account_holder
+
+    def set_account_holder(self, account_holder: str):
+        self.account_holder = account_holder
+
+    def __str__(self):
+        return f"Account {self.account_number} - {self.account_holder}, Balance: {self.balance}"
+
+    def __add__(self, amount: float):
+        self.deposit(amount)
+        return self
+
