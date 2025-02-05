331dad68d990d026309b579d32e391f420236024
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-05 14:36:04 +0600

    amount class

--- Assignment 3/PesonalAccount.py
+++ Assignment 3/PesonalAccount.py
@@ -8,4 +8,18 @@ class Amount:
         self.timestamp = datetime.now()
 
     def __str__(self):
-        return f"{self.transaction_type}: {self.value} (Дата: {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')})"
+        return f"{self.transaction_type}: {self.value} (date: {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')})"
+
+class PersonalAccount:
+    def __init__(self, account_number: int, account_holder: str):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = 0.0
+        self.transactions = []
+
+    def deposit(self, amount: float):
+        if amount <= 0:
+            raise ValueError("sum of the deposit must be greater of 0")
+        transaction = Amount(amount, "DEPOSIT")
+        self.transactions.append(transaction)
+        self.balance += amount
